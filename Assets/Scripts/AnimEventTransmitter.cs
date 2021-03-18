aa8c64d00ea1cd9007accb217aa6cbede09f1edb
Elowe <theo.leicht@gmail.com>
2021-03-18 20:59:20 +0100

    AttackDetection + bake lightmap djn2

---
+++ Assets/Scripts/AnimEventTransmitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEventTransmitter : MonoBehaviour
+{
+    public SwordTrigger ST;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void isDangerous()
+    {
+        ST.canDamage = true;
+        Debug.Log("isDangerous");
+    }
+
+    public void isntDangerous()
+    {
+        ST.canDamage = false;
+        Debug.Log("isntDangerous");
+    }
+}
