4e81406a5077ac26dc06ff506e124966bf66d73d
Elowe <theo.leicht@gmail.com>
2021-04-21 02:21:59 +0200

    Dialogue System + conversation tuto Lechecuillère/Poussierin

---
+++ Assets/Scripts/Interactable/Interactable_DialogueTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_DialogueTester : Interactable
+{
+    public ConversationInfo Conv;
+
+    public sealed override void Interact()
+    {
+        if (!this.MM.isInDialogue())
+        {
+            MM.DIALOGUE_OpenDialogue(Conv);
+        }
+    }
+}
