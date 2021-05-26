48e9eda5bc00c14f0cd0a51b3b399d9e7e999e39
Elowe <theo.leicht@gmail.com>
2021-05-26 06:02:29 +0200

    Rush p3

---
+++ Assets/Scripts/Interactable/Interactable_LootChest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Interactable_LootChest : Interactable
+{
+    private ElliotSoundSystem ESS;
+    
+
+    public RelevantEntity RE;
+
+    public GameObject Loot;
+    public int Quantity;
+
+    public GameObject SPOT_Spawn;
+    public GameObject SPOT_Destination;
+
+    public Transform Joint;
+
+    public AudioSource AsourceSimple;
+    public AudioSource AsourceEffect;
+
+
+    public sealed override void CustomStart()
+    {
+        ESS = GameObject.FindGameObjectWithTag("ElliotSoundSystem").GetComponent<ElliotSoundSystem>();
+        used = false;
+    }
+
+    public sealed override void CustomUpdate()
+    {
+        
+    }
+
+    public sealed override void Interact()
+    {
+        if (!used)
+        {
+            used = true;
+            SaveData.current.RELEVANT_KeyList = SaveData.current.RELEVANT_KeyList + RE.Key + "|";
+            AsourceSimple.Play();
+            AsourceEffect.Play();
+            Joint.DOLocalRotate(new Vector3(-70, 0, 0), 1.6f).OnComplete(() => { OpenningPart2(); });
+        }
+    }
+
+    private void OpenningPart2()
+    {
+        for(int i = 0; i< Quantity; i++)
+        {
+            GameObject loot = GameObject.Instantiate(Loot, SPOT_Spawn.transform.position , Quaternion.identity);
+            loot.transform.DOJump(SPOT_Destination.transform.position,2,1,0.7f);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Interactable/Interactable_PNJDialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_PNJDialogue : Interactable
+{
+    public LecheCuillerePermanent LCP;
+
+    public sealed override void Interact()
+    {
+        LCP.TryToInteract(MM);
+    }
+}
