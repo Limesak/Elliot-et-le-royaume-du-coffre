51be2749d66aee748c851f25e64ad0d8eaf8f3a2
Light_Unlighted <momoproduc@gmail.com>
2021-07-06 18:57:24 +0200

    Paul Scène pour Animations

---
+++ Assets/Paul_File/Test_Animation/Animator_Lutin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_Lutin : MonoBehaviour
+{
+    Animator anim;
+    public bool panic = false;
+    public bool walk = false;
+    public bool run = false;
+    public bool surprise = false;
+    public bool give = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (panic) anim.SetBool("panic", true); else anim.SetBool("panic", false);
+        if (walk) anim.SetBool("walk", true); else anim.SetBool("walk", false);
+        if (run) anim.SetBool("run", true); else anim.SetBool("run", false);
+        if (surprise) anim.SetBool("surprise", true); else anim.SetBool("surprise", false);
+        if (give) anim.SetBool("surprise", true); else anim.SetBool("give", false);
+
+    }
+}
---
+++ Assets/Paul_File/Test_Animation/Elliot_Anim_Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elliot_Anim_Script : MonoBehaviour
+{
+    Animator anim;
+    public bool walking = false;
+    public bool trotting = false;
+    public bool running = false;
+    public bool death = false;
+    public bool attack = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (walking) anim.SetBool("Walk", true); else anim.SetBool("Walk", false);
+        if (trotting) anim.SetBool("Trotting", true); else anim.SetBool("Trotting", false);
+        if (running) anim.SetBool("Sprint", true); else anim.SetBool("Sprint", false);
+        if (death) anim.SetBool("Death", true); else anim.SetBool("Death", false);
+        if (attack) anim.SetBool("Attack", true); else anim.SetBool("Attack", false);
+    }
+}
