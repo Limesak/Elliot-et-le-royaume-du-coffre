4b79753ec4dabe4f5b1ef75de638fc6f681db3db
Elowe <theo.leicht@gmail.com>
2021-07-09 17:30:54 +0200

    Music manager + debugs

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource Asource;
+
+    public AudioClip[] Musics;
+
+    public int currentIndex;
+
+    private float nextDateForChange;
+
+    void Start()
+    {
+        nextDateForChange = 0;
+        currentIndex = Random.Range(0, Musics.Length);
+    }
+
+    void Update()
+    {
+        if (nextDateForChange <= Time.time)
+        {
+            int rdm = currentIndex;
+            while (rdm == currentIndex)
+            {
+                rdm = Random.Range(0, Musics.Length);
+            }
+            currentIndex = rdm;
+
+            nextDateForChange = Time.time + Musics[currentIndex].length + 2;
+
+            Asource.PlayOneShot(Musics[currentIndex]);
+        }
+    }
+}
